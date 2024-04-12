b901e8cd75f5497cd88af417fcf325c001e9c8a6
Jorge Gonzalez Fuentes <jorgegonfue4d@gmail.com>
2024-04-12 21:42:44 +0200

    Ultimos ajustes

--- resources/js/Components/Componentes-ATP/Inicio.jsx
+++ resources/js/Components/Componentes-ATP/Inicio.jsx
@@ -1,6 +1,5 @@
 import "../../../css/app.css"
 import logo from "../../../img/Logo-Fondo/Logo.png"
-import { Link } from 'react-router-dom';
 
 function Inicio() {
 
@@ -9,6 +8,24 @@ function Inicio() {
         <main>
             <div>
                 <img src={logo} alt="Logo ArtTattooPMG" className="Logo"/>
+                <div className="info">
+                    <h1>BIENVENIDOS A LA WEB DE ARTTATTOOPMG</h1>
+                    <hr/>
+                    <p>
+                        Aquí encontrarás un lugar donde los mejores profesionales te atenderán para realizar los tatuajes que
+                        tengas pensado.<br/>
+                        <br/>
+                        Para concertar una cita, puedes ir a la pestaña de “Reservar Tu Cita”, donde encontraras a tu
+                        disposición todas las herramientas para poder tener una cita en nuestro estudio.<br/>
+                        <br/>
+                        También puedes informarte sobre los próximos eventos en los que participaremos, así como ver nuestra
+                        galería, que consiste en los trabajos que hemos realizado, o bien informarte acerca de nuestro equipo e
+                        instalaciones.<br/>
+                        <br/>
+                        Muchas gracias por su visita.
+                    </p>
+                    <a href="" className="boton">Reserva Tu Cita</a>
+                </div>
             </div>
         </main>
       </>
--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from '../Components/Componentes-ATP/Header';
 import Inicio from '../Components/Componentes-ATP/Inicio'
 import Footer from '../Components/Componentes-ATP/Footer';
@@ -8,18 +7,11 @@ import '../../css/app.css';
 function Index() {
 
   return (
-      <Router>
-          <Header />
-          {/* <Routes>
-              <Route path="/" element={<Inicio />} />
-              <Route path="/SobreNosotros" element={<SobreNosotros />} />
-              <Route path="/Galeria" element={<Galeria />} />
-              <Route path="/Eventos" element={<Eventos />} />
-              <Route path="/Reservar" element={<Reservar />} />
-          </Routes> */}
-          <Inicio />
-          <Footer />
-      </Router>
+      <>
+        <Header />
+            <Inicio />
+        <Footer />
+      </>
   );
 }
 
