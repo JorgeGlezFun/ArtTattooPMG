86c97020daec048d03f627e4dc68c5eb3a879678
Jorge Gonzalez Fuentes <jorgegonfue4d@gmail.com>
2024-04-23 23:40:33 +0200

    Creacion del contenedor de texto

---
+++ resources/js/Components/Componentes-ATP/SobreNosotros.jsx
@@ -0,0 +1,83 @@
+import "../../../css/appSN.css"
+import logo from "../../../img/Logo-Fondo/Logo.png"
+
+function SobreNosotros() {
+
+    return (
+      <>
+        <main>
+            <div className="SobreNosotros">
+                <div className="infoSN">
+                    <h1 className="titulo">¿Quiénes somos?</h1>
+                    <hr className="separadorSN"/>
+                    <div className="contenedor">
+                        <p className="textoSN">
+                        ArtTattooPMG es un negocio dedicado al mundo de los tatuajes y perforaciones, donde los mejores profesionales plasmarán en tu piel tus ideas de tatuaje y/o piercings. <br />
+                        <br />
+                        Aquí podrás encontrar una gran variedad de estilos de tatuaje que se adecuen a la idea que tienes en mente, así como de diversos métodos de perforación para realizarte el piercing que deseas. <br />
+                        <br />
+                        No esperes mas y reserva ya tu cita para concertar tu próximo tatuaje/piercing. <br />
+                        <br />
+                        ¡Te esperamos!
+                        </p>
+                        <img src={logo} alt="Logo" className="LogoSN"/>
+                    </div>
+                </div>
+                <div className="infoSN">
+                    <h1 className="titulo">La Artista</h1>
+                    <hr className="separadorSN"/>
+                    <p className="textoSN">
+                        Aquí encontrarás un lugar donde los mejores profesionales te atenderán para realizar los tatuajes que
+                        tengas pensado.<br/>
+                        <br/>
+                        Para concertar una cita, puedes ir a la pestaña de “Reservar Tu Cita”, donde encontraras a tu
+                        disposición todas las herramientas para poder tener una cita en nuestro estudio.<br/>
+                        <br/>
+                        También puedes informarte sobre los próximos eventos en los que participaremos, así como ver nuestra
+                        galería, que consiste en los trabajos que hemos realizado, o bien informarte acerca de nuestro equipo e
+                        instalaciones.<br/>
+                        <br/>
+                        Muchas gracias por su visita.
+                    </p>
+                </div>
+                <div className="infoSN">
+                    <h1 className="titulo">El Estudio</h1>
+                    <hr className="separadorSN"/>
+                    <p className="textoSN">
+                        Aquí encontrarás un lugar donde los mejores profesionales te atenderán para realizar los tatuajes que
+                        tengas pensado.<br/>
+                        <br/>
+                        Para concertar una cita, puedes ir a la pestaña de “Reservar Tu Cita”, donde encontraras a tu
+                        disposición todas las herramientas para poder tener una cita en nuestro estudio.<br/>
+                        <br/>
+                        También puedes informarte sobre los próximos eventos en los que participaremos, así como ver nuestra
+                        galería, que consiste en los trabajos que hemos realizado, o bien informarte acerca de nuestro equipo e
+                        instalaciones.<br/>
+                        <br/>
+                        Muchas gracias por su visita.
+                    </p>
+                </div>
+                <div className="infoSN">
+                    <h1 className="titulo">Nuestros Trabajos</h1>
+                    <hr className="separadorSN"/>
+                    <p className="textoSN">
+                        Aquí encontrarás un lugar donde los mejores profesionales te atenderán para realizar los tatuajes que
+                        tengas pensado.<br/>
+                        <br/>
+                        Para concertar una cita, puedes ir a la pestaña de “Reservar Tu Cita”, donde encontraras a tu
+                        disposición todas las herramientas para poder tener una cita en nuestro estudio.<br/>
+                        <br/>
+                        También puedes informarte sobre los próximos eventos en los que participaremos, así como ver nuestra
+                        galería, que consiste en los trabajos que hemos realizado, o bien informarte acerca de nuestro equipo e
+                        instalaciones.<br/>
+                        <br/>
+                        Muchas gracias por su visita.
+                    </p>
+                </div>
+            </div>
+        </main>
+      </>
+    )
+  }
+
+export default SobreNosotros
--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Header from '../Components/Componentes-ATP/Header';
 import Inicio from '../Components/Componentes-ATP/Inicio'
+import SobreNosotros from '../Components/Componentes-ATP/SobreNosotros'
 import Footer from '../Components/Componentes-ATP/Footer';
-import '../../css/app.css';
 
 function Index() {
 
   return (
       <>
         <Header />
-            <Inicio />
+            {/* <Inicio /> */}
+            <SobreNosotros />
         <Footer />
       </>
   );
