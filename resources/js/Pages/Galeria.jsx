22ff25b9dd56ccfcb38bfc8e8c63f480ac3ad9ce
Jorge Gonzalez Fuentes <jorgegonfue4d@gmail.com>
2024-05-23 23:57:20 +0200

    Creacion prototipo galeria

--- resources/js/Pages/Galeria.jsx
+++ resources/js/Pages/Galeria.jsx
@@ -4,32 +4,44 @@ import logo from "../../img/Logo-Fondo/Logo.png"
 import { Head } from '@inertiajs/react';
 
 export default function Galeria({ auth }) {
+    let filas = [];
+    for (let i = 0; i < 3; i++) {
+        let divs = [];
+        for (let j = 0; j < 4; j++) {
+            divs.push(
+                <div key={`${i}-${j}`} className='contenedorImagenGaleria'>
+                    <img src={logo} alt="Logo" />
+                </div>
+            );
+        }
+        filas.push(<div key={i} className='fila'>{divs}</div>);
+    }
+
     return (
         <>
             <Head title="Inicio" />
             <Header user={auth.user} />
-            <div className='mainInicio'>
-                <div className="info">
-                    <img src={logo} alt="Logo ArtTattooPMG" className="Logo"/>
-                    <div className="infoTexto">
-                        <h1 className="titulo">BIENVENIDOS A LA WEB DE ARTTATTOOPMG</h1>
-                        <hr className="separador"/>
-                        <p className="texto">
-                            Aquí encontrarás un lugar donde los mejores profesionales te atenderán para realizar los tatuajes que
-                            tengas pensado.<br/>
-                            <br/>
-                            Para concertar una cita, puedes ir a la pestaña de “Reservar Tu Cita”, donde encontraras a tu
-                            disposición todas las herramientas para poder tener una cita en nuestro estudio.<br/>
-                            <br/>
-                            También puedes informarte sobre los próximos eventos en los que participaremos, así como ver nuestra
-                            galería, que consiste en los trabajos que hemos realizado, o bien informarte acerca de nuestro equipo e
-                            instalaciones.<br/>
-                            <br/>
-                            Muchas gracias por su visita.
-                        </p>
-                        <div className="infoBoton">
-                            <a href="" className="boton">Reserva Tu Cita</a>
-                        </div>
+            <div className='mainGaleria'>
+                <div className="contenedorGaleria">
+                    <div className='h-full w-fit flex flex-col items-start justify-start p-4'>
+                        <h1>Estilos:</h1>
+                        <form action="" className='flex flex-col'>
+                            <div>
+                                <input type="checkbox" name="" id="" />
+                                <label htmlFor=""> A color</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" name="" id="" />
+                                <label htmlFor=""> A color</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" name="" id="" />
+                                <label htmlFor=""> A color</label>
+                            </div>
+                        </form>
+                    </div>
+                    <div className=''>
+                        {filas}
                     </div>
                 </div>
             </div>
